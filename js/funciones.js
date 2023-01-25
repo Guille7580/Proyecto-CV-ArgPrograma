8e691ac032d88cd64819bbc448f4171efa02bfb6
Guille7580 <guilleappen@hotmail.com>
2023-01-25 16:01:05 -0300

    agregado funciones y arreglos css

--- js/funciones.js
+++ js/funciones.js
@@ -1,4 +1,4 @@
-document.getElementById("linin").addEventListener("click",function () {
+document.getElementById("linke").addEventListener("click",function () {
     window.open("https://www.linkedin.com/in/guille-appe-fullstack/")
 })
 document.getElementById("mail").addEventListener("click",function () {
@@ -10,6 +10,32 @@ document.getElementById("wsp").addEventListener("click",function () {
     window.open("http://wa.me/+543533680954")
 })
 
-document.getElementById("ubic").addEventListener("click",function () {
+document.getElementById("ubica").addEventListener("click",function () {
     window.open("https://goo.gl/maps/TsqPmcDZXDt3AC5h8")
 })
+
+
+function enblanco() {
+    document.getElementById("game").style.color = '#28185c';
+}
+function enrojo() {
+    document.getElementById("game").style.color = '#5c1818';
+}
+function enblanco1() {
+    document.getElementById("pc").style.color = '#28185c';
+}
+function enrojo1() {
+    document.getElementById("pc").style.color = '#5c1818';
+}
+function enblanco2() {
+    document.getElementById("music").style.color = '#28185c';
+}
+function enrojo2() {
+    document.getElementById("music").style.color = '#5c1818';
+}
+function enblanco3() {
+    document.getElementById("futbol").style.color = '#28185c';
+}
+function enrojo3() {
+    document.getElementById("futbol").style.color = '#5c1818';
+}
